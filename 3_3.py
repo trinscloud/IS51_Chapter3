3c1460e5d6a3ef2798de04dec84b44706ed46c2a
trinscloud <trinhamm@gmail.com>
2022-04-05 15:18:54 -0700

    Modified

--- 3_3.py
+++ 3_3.py
@@ -1,1 +1,17 @@
 
+# print("This program displays a famous movie quotations.")
+# responses = ('1','2','3')
+# response = '0'
+# while response not in responses:
+#     response = input("Enter 1, 2, or, 3: ")
+#     if response == '1':
+#         print("Plastics.")
+#     elif response == '2':
+#         print("Rosebud.")
+#     elif response == '3':
+#         print ("That's all folks.")
+
+print("Enter -1 to terminate entering numbers.)")
+num = eval(input("Enter"))
+
+
